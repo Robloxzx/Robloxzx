fcda69dad934e53a6de2998a0ab352089e673f31
Robloxzx <github25519@gmail.com>
2026-04-12 08:41:42 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,14 +1,5 @@
 function openPage(id){
 
-window.open(
-"https://airconditionstrodefist.com/xexd2an06?key=8b286cd67ff1ac791222566d220cda9d",
-"_blank"
-)
-
-setTimeout(function(){
-
 window.location.href="script1.html?id="+id
-
-},3000)
 
 }
